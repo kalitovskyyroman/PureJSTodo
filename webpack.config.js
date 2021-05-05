fd892ac4336c7f1f195b62ddfd67c5fbf94a5665
Roman <roman.kalitovskyy@gmail.com>
2021-05-05 13:28:57 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
